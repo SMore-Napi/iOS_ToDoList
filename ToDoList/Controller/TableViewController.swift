b255f256f4900f0b9bcd312b89ba6fb1e34c9f2e
SMore-Napi <s.roma245@gmail.com>
2022-03-28 23:59:24 +0300

    ToDo List

--- ToDoList/Controller/TableViewController.swift
+++ ToDoList/Controller/TableViewController.swift
@@ -67,7 +67,7 @@ class ToDoTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt
        indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier:
-           "ToDoCellIdentifier", for: indexPath)
+           "ToDoCellIdentifier", for: indexPath) as! ToDoCell
         
     let toDo = toDos[indexPath.row]
     var content = cell.defaultContentConfiguration()
---
+++ ToDoList/Model/ToDoCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Роман Солдатов on 28.03.2022.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
