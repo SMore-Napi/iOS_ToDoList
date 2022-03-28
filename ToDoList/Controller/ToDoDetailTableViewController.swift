a342582ddd9b6fdd5ef02baaea113210669d74ab
SMore-Napi <s.roma245@gmail.com>
2022-03-28 23:55:18 +0300

    ToDo List

--- ToDoList/Controller/ToDoDetailTableViewController.swift
+++ ToDoList/Controller/ToDoDetailTableViewController.swift
@@ -26,8 +26,19 @@ class ToDoDetailTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dueDateDatePicker.date = Date().addingTimeInterval(24*60*60)
-        updateDueDateLabel(date: dueDateDatePicker.date)
+        let currentDueDate: Date
+        if let toDo = toDo {
+          navigationItem.title = "To-Do"
+          titleTextField.text = toDo.title
+          isCompleteButton.isSelected = toDo.isComplete
+          currentDueDate = toDo.dueDate
+          notesTextView.text = toDo.notes
+        } else {
+          currentDueDate = Date().addingTimeInterval(24*60*60)
+        }
+
+        dueDateDatePicker.date = currentDueDate
+        updateDueDateLabel(date: currentDueDate)
         updateSaveButtonState()
     }
     
@@ -99,6 +110,14 @@ class ToDoDetailTableViewController: UITableViewController {
         let isComplete = isCompleteButton.isSelected
         let dueDate = dueDateDatePicker.date
         let notes = notesTextView.text
-        toDo = ToDo(title: title, isComplete: isComplete, dueDate: dueDate, notes: notes)
+        if toDo != nil {
+                toDo?.title = title
+                toDo?.isComplete = isComplete
+                toDo?.dueDate = dueDate
+                toDo?.notes = notes
+            } else {
+                toDo = ToDo(title: title, isComplete: isComplete,
+                   dueDate: dueDate, notes: notes)
+            }
     }
 }
